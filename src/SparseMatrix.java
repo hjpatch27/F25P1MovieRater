2dddb425150fe495457b1f4e2996490b72ad52e0
hjpatch27 <hjpatch27@vt.edu>
2025-09-07 16:44:50 -0400

    Update SparseMatrix.java
    Added insert() and get() with initial comments

--- src/SparseMatrix.java
+++ src/SparseMatrix.java
@@ -6,6 +6,33 @@ import java.util.HashMap;
  * @author Henry Patch (hjpatch27), Nyssa Loeu (nyssal23)
  * @version 9.7.2025
  */
-public class SparseMatrix {
+public class SparseMatrix 
+{
+    private int rows;
+    private int cols;
+    private HashMap<Integer, HashMap<Integer, Integer>> matrixElements;
+}
 
+/**
+ * Inserts a given value into the sparse matrix at a specified
+ * row and column.
+ * @param row is the row number for the value to be inserted at.
+ * @param col is the column number for the value to be inserted at.
+ * @param value is the integer to be inserted into the spares matrix.
+ */
+public void insert(int row, int col, int value)
+{
+    
+}
+
+/**
+ * 
+ * @param row is the row number to access in the sparse matrix.
+ * @param col is the column number to access in the sparse matrix.
+ * @return int which is the value in the given row and column
+ * of the sparse matrix.
+ */
+public int get(int row, int col)
+{
+    
 }
