286bf88befd638d15a715493558720a5edf36232
hjpatch27 <hjpatch27@vt.edu>
2025-09-07 16:47:14 -0400

    Updated SparseMatrix
    Fixing error with methods outside of SparseMatrix class.

--- src/SparseMatrix.java
+++ src/SparseMatrix.java
@@ -11,28 +11,28 @@ public class SparseMatrix
     private int rows;
     private int cols;
     private HashMap<Integer, HashMap<Integer, Integer>> matrixElements;
-}
-
-/**
- * Inserts a given value into the sparse matrix at a specified
- * row and column.
- * @param row is the row number for the value to be inserted at.
- * @param col is the column number for the value to be inserted at.
- * @param value is the integer to be inserted into the spares matrix.
- */
-public void insert(int row, int col, int value)
-{
     
-}
+    /**
+     * Inserts a given value into the sparse matrix at a specified
+     * row and column.
+     * @param row is the row number for the value to be inserted at.
+     * @param col is the column number for the value to be inserted at.
+     * @param value is the integer to be inserted into the spares matrix.
+     */
+    public void insert(int row, int col, int value)
+    {
+        
+    }
 
-/**
- * 
- * @param row is the row number to access in the sparse matrix.
- * @param col is the column number to access in the sparse matrix.
- * @return int which is the value in the given row and column
- * of the sparse matrix.
- */
-public int get(int row, int col)
-{
-    
+    /**
+     * 
+     * @param row is the row number to access in the sparse matrix.
+     * @param col is the column number to access in the sparse matrix.
+     * @return int which is the value in the given row and column
+     * of the sparse matrix.
+     */
+    public int get(int row, int col)
+    {
+        return 0;
+    }
 }
