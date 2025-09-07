3377308cf47a0e20ff8076a9db08d5f58f6f1fa0
hjpatch27 <hjpatch27@vt.edu>
2025-09-07 16:24:18 -0400

    Create SparseMatrix.java

---
+++ src/SparseMatrix.java
@@ -0,0 +1,10 @@
+/**
+ * Implementation of a sparse matrix. This will be used as the data structure
+ * for our MovieRater. 
+ * 
+ * @author Henry Patch (hjpatch27), Nyssa Loeu (nyssal23)
+ * @version 9.7.2025
+ */
+public class SparseMatrix {
+
+}
