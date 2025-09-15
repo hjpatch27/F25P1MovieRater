5becb6f43c02123043eb8d15d15fcc1a1a25b87c
hjpatch27 <hjpatch27@vt.edu>
2025-09-15 16:39:30 -0400

    Updated MovieRaterTest
    Uncommented tests

--- src/MovieRaterTest.java
+++ src/MovieRaterTest.java
@@ -38,80 +38,66 @@ public class MovieRaterTest extends TestCase {
      * Test empty print movie or reviewer
      * 
      * @throws IOException
-     *
-     *             Commented out for Mutation Coverage
-     * 
-     * 
-     *             public void testRefMissing()
-     *             throws IOException
-     *             {
-     *             assertNull(it.listMovie(2));
-     *             assertNull(it.listReviewer(3));
-     *             assertFalse(it.deleteScore(5, 1));
-     *             assertFalse(it.deleteReviewer(2));
-     *             assertFalse(it.deleteMovie(2));
-     *             }
      */
+    public void testRefMissing() throws IOException
+    {
+        assertNull(it.listMovie(2));
+        assertNull(it.listReviewer(3));
+        assertFalse(it.deleteScore(5, 1));
+        assertFalse(it.deleteReviewer(2));
+        assertFalse(it.deleteMovie(2));
+    }
+     
 
     /**
      * Test insert two items and print
      * 
      * @throws IOException
-     *
-     *             Commented out for Mutation Coverage
-     * 
-     *             public void testRefinsertTwo()
-     *             throws IOException
-     *             {
-     *             assertTrue(it.addReview(2, 3, 7));
-     *             assertTrue(it.addReview(2, 5, 5));
-     *             assertFuzzyEquals(it.printRatings(), "2: (3, 7) (5, 5)");
-     *             assertFuzzyEquals(it.listMovie(3), "3: 7");
-     *             assertFuzzyEquals(it.listReviewer(2), "2: 7 5");
-     *             }
      */
+    public void testRefinsertTwo() throws IOException
+    {
+        assertTrue(it.addReview(2, 3, 7));
+        assertTrue(it.addReview(2, 5, 5));
+        assertFuzzyEquals(it.printRatings(), "2: (3, 7) (5, 5)");
+        assertFuzzyEquals(it.listMovie(3), "3: 7");
+        assertFuzzyEquals(it.listReviewer(2), "2: 7 5");
+    }
+     
 
     /**
      * Test bad review values
      * 
      * @throws IOException
-     *
-     *             Commented out for Mutation Coverage
-     * 
-     *             public void testRefBadRatings()
-     *             throws IOException
-     *             {
-     *             assertFalse(it.addReview(2, 3, -1));
-     *             assertFalse(it.addReview(2, 4, 0));
-     *             assertFalse(it.addReview(2, 5, 20));
-     *             assertFuzzyEquals(it.printRatings(), "");
-     *             }
      */
+    public void testRefBadRatings() throws IOException
+    {
+        assertFalse(it.addReview(2, 3, -1));
+        assertFalse(it.addReview(2, 4, 0));
+        assertFalse(it.addReview(2, 5, 20));
+        assertFuzzyEquals(it.printRatings(), "");
+     }
+     
 
     /**
      * Test insert 5 items and print
      * 
      * @throws IOException
-     *
-     *             Commented out for Mutation Coverage
-     * 
-     *             public void testRefinsertFive()
-     *             throws IOException
-     *             {
-     *             assertTrue(it.addReview(7, 3, 10));
-     *             assertTrue(it.addReview(2, 3, 7));
-     *             assertTrue(it.addReview(3, 5, 8));
-     *             assertTrue(it.addReview(5, 7, 9));
-     *             assertTrue(it.addReview(7, 7, 1));
-     *             assertFuzzyEquals(
-     *             multiline(
-     *             "2: (3, 7)",
-     *             "3: (5, 8)",
-     *             "5: (7, 9)",
-     *             "7: (3, 10) (7, 1)"),
-     *             it.printRatings());
-     *             }
      */
+    public void testRefinsertFive() throws IOException
+    {
+        assertTrue(it.addReview(7, 3, 10));
+        assertTrue(it.addReview(2, 3, 7));
+        assertTrue(it.addReview(3, 5, 8));
+        assertTrue(it.addReview(5, 7, 9));
+        assertTrue(it.addReview(7, 7, 1));
+        assertFuzzyEquals(
+            multiline(
+                "2: (3, 7)",
+                "3: (5, 8)",
+                "5: (7, 9)",
+                "7: (3, 10) (7, 1)"),
+                it.printRatings());
+    }
 
     /**
      * Test the addReview() method
